0ac4297af22a7d50366201e6586e227e0342ff21
Frank Bwana <bwanafranklin@gmail.com>
2023-07-05 13:30:29 +0300

    first commit - commit all files to repo

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import { userRouter } from './routes/userRoutes.js';
+
+
+const app = express()
+
+app.use(express.json());
+app.use(cors());
+app.use("/auth", userRouter)
+
+mongoose.connect("mongodb+srv://bwanafrank:logins12@logins.19garo9.mongodb.net/logins?retryWrites=true&w=majority")
+
+app.listen(5000, () => console.log(`SERVER STARTS on Port 5000`));
